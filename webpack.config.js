c072d66dd92e4ab1b998285ac23ac219ba72887c
javier vilchis <javivilchis@gmail.com>
2021-10-13 12:05:02 -0500

    addin stuff

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+
+const config = {
+  entry: "./public/index.js",
+  output: {
+    path: __dirname + "/public/dist",
+    filename: "bundle.js"
+  },
+  mode: "development",
+  plugins: [
+    new WebpackPwaManifest({
+      name: "The Budget App",
+      short_name: "Dbudget App",
+      description: "An application for keeping track of your expenses.",
+      background_color: "#01579b",
+      theme_color: "#ffffff",
+      "theme-color": "#ffffff",
+      start_url: "/",
+      icons: [
+        {
+          src: path.resolve("public/icons/icon-192x192.png"),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join("assets", "icons")
+        }
+      ]
+    })
+  ],
+  // configure webpack to use babel-loader to bundle our separate modules and transpile the code
+  // refer to https://github.com/babel/babel-loader for more information on the settings
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // files must end in ".js" to be transpiled
+        exclude: /node_modules/, // don't transpile code from "node_modules"
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      }
+    ]
+  }
+};
+
+module.exports = config;
